20fb21934ac0488a3229d85b356a2e1c01d83ae7
Memaybew-1 <memaybewv1@gmail.com>
2026-08-28 21:24:46 +0700

    Delete menu.js

--- menu.js
+++
@@ -1,9 +0,0 @@
-const btn = document.createElement('a');
-link_um.id = 'link_um';
-link_um.textContent = 'Người Um';
-
-document.body.appendChild(link_um);
-
-btn.addEventListener('click', () => {
-    console.log('Button clicked');
-});
